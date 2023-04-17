ffc49e4cb741718ca93e2c6e6c82a0b52cf368e8
TadaPrin <n.toesier@gmail.com>
2023-04-17 10:06:14 +0700

    Excercise4
    Excercise4 in CP3 course

---
+++ Excercisse4_Prin_T.py
@@ -0,0 +1,14 @@
+'''
+Excercise4 homework -CP3 course by Born to Dev school-
+'''
+
+FE = 88.5 #Foundation English
+GE = 76 #General Business
+ICS = 78 #Introduction to Computer Systems
+COP =80.5 #Computer Programming
+print("Enjoy your score")
+
+print(FE)
+print(GE)
+print(ICS)
+print(COP)
