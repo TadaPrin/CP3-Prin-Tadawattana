ec469c13a1087cda296b53f82c60bb05fbde2406
TadaPrin <n.toesier@gmail.com>
2023-04-19 10:14:20 +0700

    Homework Lecture50

---
+++ Lecture50.py
@@ -0,0 +1,15 @@
+'''
+Create function for sum, minus, multipy, and divide
+'''
+x = int(input("Fill the value of x : "))
+y = int(input("Fill the value of y : "))
+def SumFunction(x,y):
+    print(x+y)
+def MinusFunction(x,y):
+    print(x-y)
+def MultipyFunction(x,y):
+    print(x*y)
+def DivideFunction(x,y):
+    print(x/y)
+
+MinusFunction(x,y)
